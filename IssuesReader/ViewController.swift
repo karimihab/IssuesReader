5190fcc9cede10e75f24f5bb2388b1c28f9a7261
Karim Ihab <karim.ihab@gmail.com>
2019-07-26 13:53:52 +0200

    Initial Commit

---
+++ IssuesReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IssuesReader
+//
+//  Created by Karim Ihab on 26/07/2019.
+//  Copyright © 2019 rabobank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
