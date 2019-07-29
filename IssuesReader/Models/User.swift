11f1aa552b72f1178175ab702fe12d7bafdb2ced
Karim Ihab <karim.ihab@gmail.com>
2019-07-29 11:11:52 +0200

    - Initial Commit - Project Setup -

---
+++ IssuesReader/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  IssuesReader
+//
+//  Created by Karim Ihab on 29/07/2019.
+//  Copyright © 2019 rabobank. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+	let firstName: String
+	let lastName: String
+	let birthDate: String
+	let issuesCount: Int
+}
